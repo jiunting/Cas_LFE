145223f83d80b8435ad3dafa650d22ff66e4b74d
JTim_Talapas <jiunting@talapas-ln1.cm.cluster>
2021-09-20 21:42:08 -0700

    add detc2PhaseLink.py

---
+++ detc2PhaseLink.py
@@ -0,0 +1,50 @@
+# convert all the detection output (daily2input.py) to phase_link PhaseLink prediction file
+
+import pandas as pd
+import glob
+
+#
+csv_path = '/projects/amt/jiunting/Cascadia_LFE/Detections_S'
+outfile = 'CasLFEs_S.out'
+thres = 0.1 #minumum y value
+
+
+csvs = glob.glob(csv_path+'/'+'cut_daily_*.csv')
+
+all_data = []
+for csv in csvs:
+    tmp_csv = pd.read_csv(csv)
+    all_data.append(tmp_csv)
+
+all_data = pd.concat(all_data)
+all_data.reset_index(drop=True, inplace=True)
+
+'''
+Example format
+network                                     PO
+sta                                       TWGB
+chn                                         HH
+stlon                                 -124.256
+stlat                                  48.6076
+stdep                                      127
+starttime          2005-01-01T07:11:00.000000Z
+endtime            2005-01-01T07:11:14.990000Z
+y                                         0.51
+idx_max_y                                   54
+id           PO.TWGB.HH_2005-01-01T07:11:00.54
+'''
+
+# apply filter
+idx = all_data[all_data['y']>=thres].index
+
+# writing output
+OUT1 = open(outfile,'w')
+Nper = len(idx)//10000
+for i in idx:
+    if i%Nper==0:
+        OUT2 = open('tmplog.out','a')
+        OUT2.write('Now at %d total of %d (%f) \n'%(i,len(idx), (i/len(idx))*100  ))
+        OUT2.close()
+    OUT1.write('%s %s %s %s %.3f %.1f\n'%(all_data.iloc[i].network,all_data.iloc[i].sta,'S',all_data.iloc[i].id.split('_')[-1],all_data.iloc[i].y,0.5))
+
+OUT1.close()
