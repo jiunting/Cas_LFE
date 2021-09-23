5195a95fb99a63fabb34dc1b0e8077cfca6a07c9
JTim_Talapas <jiuntim.lin@gmail.com>
2021-09-23 14:32:29 -0700

    modify PhaseLink_classification.py: residual calculation using upper triangle without diagonal self values

--- PhaseLink_classification.py
+++ PhaseLink_classification.py
@@ -2,6 +2,7 @@
 
 import numpy as np
 from obspy import UTCDateTime
+import copy
 
 
 EQinfo = np.load('sav_family_phases.npy',allow_pickle=True) #Note! detection file has a shift for each family
@@ -55,6 +56,14 @@ for fam in EQinfo.keys():
         
 np.save('diff_travelTime_table.npy',sav_table)    
 
+# keep only table value
+sav_table_value = []
+sav_table_fam = []
+for k in sav_table.keys():
+    sav_table_value.append(sav_table[k]['table'])
+    sav_table_fam.append(k)
+
+sav_table_value, sav_table_fam = np.array(sav_table_value), np.array(sav_table_fam)
 
 '''
 #======= plot the checking table ========
@@ -84,29 +93,156 @@ SNB    CN   ?    ? S      ? 20050907 0754 16.3000 GAU  1.00e-01 -1.00e+00 -1.00e
 '''
 
 
-#IN1 = open('../PhaseLink/CasLFEs_S_y0.1.nlloc','r')
-filename = '../PhaseLink/CasLFEs_S_y0.1.nlloc'
-sav_sta = []
-sav_phs = []
-sav_t = []
+IN1 = open('../PhaseLink/CasLFEs_S_y0.1.nlloc','r')
+#filename = '../PhaseLink/CasLFEs_S_y0.7.nlloc'
+#filename = './test.nlloc'
+filt_N = 3 # minimum number of observations 
 
+sav_sta_phase = []
+sav_t_part = [] # only have partial t, not the full sav_t
+#sav_table_obs = []
+
+OUT1 = open('family_classify_S_y0.1.txt','w')
+OUT1.write('FamilyID  mean_residual  first_arr  N_obs  max_N_obs\n')
 with open(filename,'r') as IN1:
     for line in IN1:
         if len(line.split())!=14:
             # this is header line
-            sav_sta = []
-            sav_phs = []
-            sav_t = []
+            if len(sav_sta_phase)!=0:
+                match_fam(sav_sta_phase,sav_t_part,OUT1) # this done all the work below
+                '''
+                # deal with previous stuff
+                sav_sta_phase = np.array(sav_sta_phase)
+                sav_t = [] # full t, same length as sta_list * phase_list
+                for sta in sta_list:
+                    for phs in phase_list:
+                        if '_'.join([sta,phase_conv[phs]]) in sav_sta_phase:
+                            idx = np.where('_'.join([sta,phase_conv[phs]])==sav_sta_phase )[0][0]
+                            sav_t.append(sav_t_part[idx]-UTCDateTime("20000101"))
+                        else:
+                            sav_t.append(np.nan)
+                # convert arrival time to differential arrival time
+                sav_t = np.array(sav_t)
+                arr_diff = np.zeros([len(sav_t),len(sav_t)]) #initialize the 2D diff arrival time array
+                for i in range(len(sav_t)):
+                    arr_diff[i] = sav_t-sav_t[i]
+                #sav_table_obs.append(arr_diff) # dont want to save everything
+                #=== start differential arr matching (compare with table sav_table)===
+                residuals = np.abs(arr_diff - sav_table_value) # residuals between current obs and 130 families
+                sav_mean = [] #mean of the misfit*2 value
+                sav_n_obs = [] # number of observations
+                for ii in range(len(residuals)):
+                    tmp_residuals = residuals[ii].reshape(-1)
+                    idx_val = np.where( ~np.isnan(tmp_residuals) )[0]
+                    if len(idx_val)!=0:
+                        tmp_mean = tmp_residuals[idx_val].mean()
+                        sav_mean.append(tmp_mean)
+                        sav_n_obs.append(len(idx_val)**0.5)
+                    else:
+                        sav_mean.append(float("inf")) # or append another nan
+                        sav_n_obs.append(0)
+                # find the best fit family
+                sav_mean = np.array(sav_mean)
+                sav_n_obs = np.array(sav_n_obs)
+                idx_gt1sta = np.where(sav_n_obs>=filt_N)[0] # remove 0 and 1(itself) station match
+                if len(idx_gt1sta)==0:
+                    # not enough stations left
+                    OUT1.write('%s  %f  %s  %d  %d\n'%('000',0,"2000-01-01T00:00:00.00",0,sav_n_obs.max()))
+                    continue
+                sav_mean = sav_mean[idx_gt1sta]
+                sav_n_obs = sav_n_obs[idx_gt1sta]
+                tmp_table_fam = sav_table_fam[idx_gt1sta]
+                fam_idx = np.where( sav_mean==sav_mean.min() )[0][0]
+                #========= write to file ==============
+                OUT1.write('%s  %f  %s  %d  %d\n'%(tmp_table_fam[fam_idx],sav_mean[fam_idx],first_t.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-4],sav_n_obs[fam_idx],sav_n_obs.max()))
+                # previous stuff done! 
+                '''
+            # reset list for the next event
+            sav_sta_phase = []
+            sav_t_part = []
             continue
         sta = line.split()[0]
         phs = line.split()[4]
         t = UTCDateTime(line.split()[6]+'T'+line.split()[7]) + float(line.split()[8])
-        sav_sta.append(sta)
-        sav_phs.append(phs)
-        sav_t.append(t)
+        if sav_sta_phase==[]:
+            import copy
+            first_t = copy.deepcopy(t)
+        sav_sta_phase.append('_'.join([sta,phs]))
+        sav_t_part.append(t)
+
+if len(sav_sta_phase)!=0:
+    match_fam(sav_sta_phase,sav_t_part,OUT1)
+
+
+OUT1.close()
 
 
 
+
+def match_fam(sav_sta_phase,sav_t_part,OUT):
+    '''
+        Input:
+            sav_sta_phase:         list of station_phase
+            sav_t_part:            list of absolute arrival time
+            OUT:                   output file handle
+        Output:
+            write to OUT handle
+    '''
+    sav_sta_phase,sav_t_part = np.array(sav_sta_phase), np.array(sav_t_part)
+    sav_t = [] # full t, same length as sta_list * phase_list
+    for sta in sta_list:
+        for phs in phase_list:
+            if '_'.join([sta,phase_conv[phs]]) in sav_sta_phase:
+                idx = np.where('_'.join([sta,phase_conv[phs]])==sav_sta_phase )[0][0]
+                sav_t.append(sav_t_part[idx]-UTCDateTime("20000101"))
+            else:
+                sav_t.append(np.nan)
+    # convert arrival time to differential arrival time
+    sav_t = np.array(sav_t)
+    arr_diff = np.zeros([len(sav_t),len(sav_t)]) #initialize the 2D diff arrival time array
+    for i in range(len(sav_t)):
+        arr_diff[i] = sav_t-sav_t[i]
+    #sav_table_obs.append(arr_diff) # dont want to save everything
+    #=== start differential arr matching (compare with table sav_table)===
+    #residuals = np.abs(arr_diff - sav_table_value) # residuals between current obs and 130 families
+    #===== only take the upper triangle, and without diagonal(self) term=========
+    sav_table_value_triu = np.array([tmp_tab[np.triu_indices(len(sav_t),k=1)]  for tmp_tab in sav_table_value])
+    residuals = np.abs(arr_diff[np.triu_indices(len(sav_t),k=1)] - sav_table_value_triu ) # residuals between current obs and 130 families
+    sav_mean = [] #mean of the misfit value
+    sav_n_obs = [] # number of observations
+    for ii in range(len(residuals)):
+        tmp_residuals = residuals[ii].reshape(-1)
+        idx_val = np.where( ~np.isnan(tmp_residuals) )[0]
+        if len(idx_val)!=0:
+            tmp_mean = tmp_residuals[idx_val].mean()
+            sav_mean.append(tmp_mean)
+            #sav_n_obs.append(len(idx_val)**0.5)
+            sav_n_obs.append(len(idx_val))
+        else:
+            sav_mean.append(float("inf")) # or append another nan
+            sav_n_obs.append(0)
+    # find the best fit family
+    sav_mean = np.array(sav_mean)
+    sav_n_obs = np.array(sav_n_obs)
+    idx_gtNsta = np.where(sav_n_obs>=filt_N)[0] # minimum number of observations
+    if len(idx_gtNsta)==0:
+        # not enough stations left
+        OUT.write('%s  %f  %s  %d  %d\n'%('000',0,"2000-01-01T00:00:00.00",0,sav_n_obs.max()))
+        return      
+    sav_mean = sav_mean[idx_gtNsta]
+    sav_n_obs = sav_n_obs[idx_gtNsta]
+    tmp_table_fam = sav_table_fam[idx_gtNsta]
+    fam_idx = np.where( sav_mean==sav_mean.min() )[0][0]
+    #========= write to file ==============
+    OUT.write('%s  %f  %s  %d  %d\n'%(tmp_table_fam[fam_idx],sav_mean[fam_idx],first_t.strftime('%Y-%m-%dT%H:%M:%S.%f')[:-4],sav_n_obs[fam_idx],sav_n_obs.max()))
+    return
+
+
+
+
+
+
+OUT1.close()
 
 
 
