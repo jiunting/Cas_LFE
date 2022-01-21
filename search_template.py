38a50d2125a0152f93557dbbb84009541ab3b950
JTim_Talapas <jiuntim.lin@gmail.com>
2022-01-20 16:48:19 -0800

    feat search_template.py: 1. early-stopping of matching, 2. community detection

--- search_template.py
+++ search_template.py
@@ -73,6 +73,53 @@ def data_cut(Data1,Data2='',t1=UTCDateTime("20100101"),t2=UTCDateTime("20100102"
     #assert len(DD[0].data)==1500, "cut data not exactly 3001 points"
     return DD[0].data
 
+from obspy.signal.cross_correlation import correlate_template
+
+def CCC_QC(data1,data2):
+    #cross-correlation "Coef" cunction for QC
+    CCCF=correlate_template(data1,data2)
+    return np.max(np.abs(CCCF))
+
+def QC(data,Type='data'):
+    '''
+        quality control of data
+        return true if data pass all the checking filters, otherwise false
+        the same function used before but with a N2=90 instead of 30
+    '''
+    #nan value in data check
+    if np.isnan(data).any():
+        return False
+    #if they are all zeros
+    if np.max(np.abs(data))==0:
+        return False
+    #normalize the data to maximum 1
+    data = data/np.max(np.abs(data))
+    #set QC parameters for noise or data
+    if Type == 'data':
+        N1,N2,min_std,CC = 30,90,0.01,0.98
+    else:
+        N1,N2,min_std,CC = 30,90,0.05,0.98
+    #std window check, std too small probably just zeros
+    wind = len(data)//N1
+    for i in range(N1):
+        #print('data=',data[int(i*wind):int((i+1)*wind)])
+        #print('std=',np.std(data[int(i*wind):int((i+1)*wind)]))
+        if np.std(data[int(i*wind):int((i+1)*wind)])<min_std :
+            return False
+    #auto correlation, seperate the data into n segments and xcorr with rest of data(without small data) to see if data are non-corh
+    wind = len(data)//N2
+    for i in range(N2):
+        data_small = data[int(i*wind):int((i+1)*wind)] #small segment
+        data_bef = data[:int(i*wind)]
+        data_aft = data[int((i+1)*wind):]
+        data_whole = np.concatenate([data_bef,data_aft])
+        curr_CC = CCC_QC(data_whole,data_small)
+        if curr_CC>CC:
+            return False
+    return True
+
+
+
 '''
 def cal_CCC(data1,data2):
     #cross-correlation "Coef" cunction, return the max CC
@@ -95,9 +142,13 @@ def cal_CCC(data1,data2):
 '''
 
 @jit(nopython=True)
-def norm_data(data1,data2):
+def norm_data(data,pos=1):
     #for normalized cross-correlation
-    return (data1-np.mean(data1))/(np.std(data1)*len(data1)), (data2-np.mean(data2))/(np.std(data2))
+    # pos=1 or 2: input data is first or second
+    if pos==1:
+        return (data-np.mean(data))/(np.std(data)*len(data))
+    else:
+        return (data-np.mean(data))/(np.std(data))
 
 
 def cal_CCC(data1,data2):
@@ -165,42 +216,56 @@ def roll_zeropad(a, shift, axis=None):
         return res
 
 
-def batch_CC(n_i,i,target_keys,file_hdf5):
+def batch_CC(n_i,i,target_keys,file_hdf5,search_params):
     # do CC calculations w.r.t. a template i.g. data1
     # template is an np array
     # loopable target_keys for target data
+    ## ----------params input-------------
+    group_CC = search_params['group_CC']
+    time_max = search_params['time_max']
+    group_max = search_params['group_max']
+    #-------------------------------------
     hf = h5py.File(file_hdf5,'r')
     template = np.array(hf['data/'+i]).reshape(-1)
     group = {}
-    sav_CC = {n_i:[]}
+    #sav_CC = {n_i:[]} #save all the CC for checking
+    data1_nor = norm_data(template,pos=1) #normalization for CC calculation
+    T0 = UTCDateTime(i.split('_')[-1])
+    Tmax = T0 + (time_max*365*86400) # year to sec
     for n_j,j in enumerate(target_keys):
         if n_j<=n_i:
             continue
+        if UTCDateTime(j.split('_')[-1])>Tmax:
+            break # j is chronologically ordered, no need to search next j for this template
+        if group!={}:
+            if len(group[n_i]['group'])>=group_max:
+                break
         data2 = np.array(hf['data/'+j]).reshape(-1)
-        data1,data2 = norm_data(template,data2)
-        CC,shift = cal_CCC(data1,data2)
-        sav_CC[n_i].append(CC)
+        data2_nor = norm_data(data2,pos=2)
+        CC,shift = cal_CCC(data1_nor,data2_nor)
+        #sav_CC[n_i].append(CC)
         if CC>=group_CC:
             #print("=============")
             #print(i,j)
             #print("CC=",CC)
             # add in the group
-            #group_rec.add(n_i)
-            #group_rec.add(n_j)
             # create a new group or merge into the existing group
             if n_i in group:
                 group[n_i]['group'].append(n_j)
-                tmp = group[n_i]['template']
-                data1, _ = norm_data(tmp,data2) #data2 has been normalized earlier
-                CC,shift = cal_CCC(tmp,data2)
-                tmp = roll_zeropad(tmp,-int(0.1*shift)) + roll_zeropad(np.array(hf['data/'+j]).reshape(-1),int(0.9*shift))
-                group[n_i]['template'] = tmp/np.max(np.abs(tmp))
+                #tmp = group[n_i]['template']
+                #_,shift = cal_CCC(tmp,data2_nor) #note that to save calculation, tmp is not normalized. i.e. only want shift here
+                #tmp = roll_zeropad(tmp,-round((1/len(group[n_i]['group']))*shift)) + roll_zeropad(data2/np.max(np.abs(data2)),round((1-1/len(group[n_i]['group']))*shift))
+                #group[n_i]['template'] = tmp/np.max(np.abs(tmp))
+                #group[n_i]['template'].append(data2)
+                #group[n_i]['CC'].append(CC)
             else:
-                tmp = roll_zeropad(np.array(hf['data/'+i]).reshape(-1),-int(0.5*shift)) + roll_zeropad(np.array(hf['data/'+j]).reshape(-1),int(0.5*shift))
-                tmp = tmp/max(np.abs(tmp))
-                group[n_i] = {'group':[n_i,n_j],'template':tmp}
+                #tmp = roll_zeropad(template/np.max(np.abs(template)),-round(0.5*shift)) + roll_zeropad(data2/np.max(np.abs(data2)),round(0.5*shift))
+                #tmp = tmp/np.max(np.abs(tmp))
+                #group[n_i] = {'group':[n_i,n_j],'template':tmp}
+                #group[n_i] = {'group':[n_i,n_j],'template':[template,data2],'CC':[1,CC]}
+                group[n_i] = {'group':[n_j]}
     hf.close()
-    return group,sav_CC
+    return group #,sav_CC
     
 
 
@@ -217,7 +282,7 @@ def batch_CC(n_i,i,target_keys,file_hdf5):
 
 
 
-def make_template(csv_file_path,load=False):
+def make_template(csv_file_path,search_params,load=False,pre_QC=True):
     '''
         read the arrival from csv then
             load=False:  cut time series from the daily data centered on the arrival time
@@ -229,6 +294,11 @@ def make_template(csv_file_path,load=False):
     window_R = 15
     data_path_base = "/projects/amt/shared/cascadia_"
     file_hdf5_base = "./template"
+    #----------------------------------------------------------------------
+    ## -----search parameters----------------------------------------------
+    y_min = search_params['y_min']
+    group_CC = search_params['group_CC']
+    ncores = search_params['ncores']
     #----------------------------------------------------------------------
     csv = pd.read_csv(csv_file_path)
     if len(csv)==0:
@@ -330,19 +400,29 @@ def make_template(csv_file_path,load=False):
 #    group_rec = set()
     time_1 = time.time()
     filt_csv = csv[csv['y']>=y_min]
-    ncores = 16
-    print(' number of traces=%d, may take %f min to calculate...'%(len(filt_csv),(len(filt_csv)**2/2)*0.001/60/ncores ))
-    results = Parallel(n_jobs=ncores,verbose=10,backend='loky')(delayed(batch_CC)(n_i,i,filt_csv['id'],file_hdf5) for n_i,i in enumerate(filt_csv['id'])  )
+    print(' number of traces=%d after y filter'%(len(filt_csv)))
+    if pre_QC:
+        QC_idx = []
+        hf = h5py.File(file_hdf5,'r')
+        results = Parallel(n_jobs=ncores,verbose=0,backend='loky')(delayed(QC)(np.array(hf['data/'+tmp_id]).reshape(-1)) for idx,tmp_id in enumerate(filt_csv['id']) )
+        results = np.array(results)
+        QC_idx = np.where(results==True)
+        hf.close()
+        filt_csv = filt_csv.iloc[QC_idx]
+        print(' number of traces=%d after QC filter'%(len(filt_csv)))
+    print(' number of traces=%d, may take maximum %f min to calculate...'%(len(filt_csv),(len(filt_csv)**2/2)*0.01/60/ncores ))
+    results = Parallel(n_jobs=ncores,verbose=10,backend='loky')(delayed(batch_CC)(n_i,i,filt_csv['id'],file_hdf5,search_params) for n_i,i in enumerate(filt_csv['id'])  )
+    # collect the results from the above loops
     group = {}
-    sav_CC = []
-    for res,cc in results:
+    #sav_CC = []
+    for res in results:
         group.update(res)
-        for k in cc.keys():
-            sav_CC.append(cc[k])
+        #for k in cc.keys():
+        #    sav_CC.append(cc[k])
     
     sta = csv_file_path.split('.')[1]
     np.save('CC_group_%s.npy'%(sta),group)
-    np.save('sav_CC_%s.npy'%(sta),sav_CC)
+    #np.save('sav_CC_%s.npy'%(sta),sav_CC)
     return
 #    for n_i,i in enumerate(filt_csv['id']):
 #        print("current i=%s  (%d/%d) "%(i,n_i,len(filt_csv)))
@@ -386,29 +466,39 @@ def make_template(csv_file_path,load=False):
 
 
 
-csvs_file_path = "/projects/amt/jiunting/Cascadia_LFE/Detections_S"
+
+def run_loop(csv_file_path,search_params):
+    print('-----------------------------')
+    print('Now in :',csv_file_path)
+    make_template(csv_file_path,search_params,load=True,pre_QC=True) #before looping through all traces, QC again
+
+
+
+
+csvs_file_path = "/Users/jtlin/Documents/Project/Cascadia_LFE/Detections_S_small"
 #csvs_file_path = glob.glob(csvs_file_path+"/"+"cut_daily_*GOWB*.csv")
 csvs_file_path = glob.glob(csvs_file_path+"/"+"cut_daily_*.csv")
 csvs_file_path.sort()
 
-def run_loop(csv_file_path):
-    print('-----------------------------')
-    print('Now in :',csv_file_path)
-    make_template(csv_file_path,load=True)
+search_params = {
+    'y_min':0.2,         # CNN pick threshold value
+    'group_CC':0.2,      # threshold of template-target CC to be considered a group
+    'group_percent':0.5, # at least N[0~1] overlap can be considered a group e.g. temp1=1, detc1=[1,3,5,6,7,9]. If temp2=2, detc2=[2,3,5,11], then len(detc2 in detc1)/len(detc1)=2/6, so temp2 is not in the group
+    'time_max':1,        # maximum time span [year] for template matching. Given the short LFE repeat interval, LFEs should already occur multiple times
+    'group_max':50,     # stop matching when detecting more than N events in a same template
+    'ncores':16,
+}
 
 
-
-y_min = 0.2
-group_CC = 0.2
 # multi-processing is deployed only in the calculation, just loop each station
 for csv_file_path in csvs_file_path:
-    run_loop(csv_file_path)
+    run_loop(csv_file_path,search_params)
 
 # parallel processing for each stations
 #results = Parallel(n_jobs=2,verbose=10)(delayed(run_loop)(csv_file_path) for csv_file_path in csvs_file_path  )
 
 
-
+'''
 # plotting results
 groups = np.load('CC_group_GOWB.npy',allow_pickle=True)
 groups = groups.item()
@@ -419,8 +509,64 @@ for i in range(n):
     corr[i,i+1:n] = CCs[i]
     corr[i+1:n,i] = CCs[i]
 
-
 mask = np.triu(np.ones_like(corr, dtype=bool))
 sns.heatmap(corr, mask=mask,square=True,vmin=0,vmax=0.7, cbar_kws={"shrink": .5})
 plt.show()
+'''
 
+
+# community detection
+groups = np.load('CC_group_GOWB.npy',allow_pickle=True)
+groups = groups.item()
+import networkx as nx
+G = nx.Graph()
+
+for i,k in enumerate(groups.keys()):
+    elist = [(k,g) for g in groups[k]['group']]
+    G.add_edges_from(elist)
+
+
+# trim the graph by number of connections
+thres_conn = max([G.degree[n] for n in G.nodes])
+rate = 0.3
+max_iter = 10
+for iter in range(max_iter):
+    #thres_conn *= rate
+    thres_conn = 20
+    rm_n = []
+    for n in G.nodes:
+        if G.degree[n]<round(thres_conn):
+            rm_n.append(n)
+    print(' number of nodes=',len(G.nodes))
+    print('   remove nodes=',len(rm_n))
+    for n in rm_n:
+        G.remove_node(n)
+
+
+nx.draw(G,with_labels=False,node_size=6,edge_color=[0.8,0.8,0.8])
+plt.show()
+
+
+from networkx.algorithms import community
+#k = 6 #groups
+#GG = community.asyn_fluidc(G,k=k)
+#GG = [next(GG) for i in range(k)]
+#GG1 = next(GG)
+#GG2 = next(GG)
+#GG3 = next(GG)
+
+#GG = community.girvan_newman(G) # very slow for large network
+GG = community.greedy_modularity_communities(G)
+
+# set different color
+color_palette = sns.color_palette("tab10",len(GG))
+
+colors = [] # color for plotting each community
+for n in G:
+    for ig,g in enumerate(GG):
+        if n in g:
+            colors.append(color_palette[ig])
+
+my_pos = nx.spring_layout(G, seed = 0) # set the position so that plot will be the same
+nx.draw(G,pos=my_pos,node_color=colors,with_labels=False,node_size=3,width=0.1,edge_color=[0.8,0.8,0.8])
+plt.show()
